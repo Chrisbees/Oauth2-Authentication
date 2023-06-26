1db1752a74e47770fc159e4c7b8626e61c1db115
Chrisbees <beesworld1@yahoo.com>
2023-06-26 17:57:58 +0100

    first commit

---
+++ src/main/java/com/chrisbees/Oauthauthorizationserversocailmedia/OauthAuthorizationServerSocailMediaApplication.java
@@ -0,0 +1,13 @@
+package com.chrisbees.Oauthauthorizationserversocailmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerSocailMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerSocailMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisbees/Oauthauthorizationserversocailmedia/OauthAuthorizationServerSocailMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisbees.Oauthauthorizationserversocailmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerSocailMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
